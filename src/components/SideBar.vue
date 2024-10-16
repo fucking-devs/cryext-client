f83e9962fb037f361edf77606b51091ecee0b375
magomed-r <magomed-radzhabov@internet.ru>
2024-10-16 21:13:23 +0300

    add header and sidebar

---
+++ src/components/SideBar.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup></script>
+
+<template>
+    <div class="sidebar">
+        <div class="sidebar-item">Profile</div>
+        <div class="sidebar-item">Settings</div>
+        <div class="sidebar-item">Logout</div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.sidebar {
+    width: 300px;
+    height: 100vh;
+    background-color: var(--sidebar-color);
+
+    position: fixed;
+    left: 0;
+    top: 0;
+    bottom: 0;
+
+    z-index: 3;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+
+    .sidebar-item {
+        cursor: pointer;
+        color: var(--link-mute);
+
+        font-size: 30px;
+
+        transition: all 0.3s;
+
+        &:hover {
+            color: var(--vt-c-white);
+            text-shadow: 0 0 6px #c5aeae;
+        }
+    }
+}
+</style>
