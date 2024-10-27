43de1ccad715f95d9d7f62c67289fa1f30c33d63
Ebal vas vsex <netynety@gmail.com>
2024-10-27 22:06:41 +0400

    src/components/NewsTop.vue

---
+++ src/components/NewsTop.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <div class="container">
+ <h1>Новости криптовалют </h1>
+<p>Nansen: капитализация GameFi превысит $300 млрд к 2030 году
+ <a href="https://news.garantex.org/nansen-kapitalizatsiya-gamefi-300-mlrd-2030" >жопа</a></p>
+ <nav>Разблокировка токенов на $1,413 млрд может замедлить рост крипторынка
+    <a href="https://news.garantex.org/razblokirovka-tokenov-ugroza-dlya-kriptorinka/">xyi moi</a>
+   </nav>
+   <html> 
+    По данным CryptoRank, Base занимает первое место в рейтинге блокчейнов второго уровня (L2) по числу адресов, ежедневно участвующих в транзакциях.
+За последние 30 дней количество активных Base-кошельков составило около 1,43 млн.
+     <a href="https://news.garantex.org/base-lider-blokcheynov-l2"> sosite</a>
+   </html>
+</div>
+<div class="html"> </div>
+    </template>
+    <style>
+   h1{
+    display: inline-block;
+    color: aquamarine;
+    position: fixed;
+  bottom: 45%;
+  right: 7%;
+  font-size:x-large;
+   }
+   .container {
+        border:double rgb(152, 172, 64); 
+        padding-right: 49%; 
+        width: 300px;
+        height: 310px;
+       position: fixed;
+    }
+p {
+    display: inline-block;
+    color: brown;
+    position: fixed;
+  bottom: 40%;
+  right: 7%;
+}
+nav {
+    display: inline-block;
+    position: fixed;
+  bottom: 35%;
+  right: 7%;
+  color: chartreuse;
+}
+.html {
+    display: inline-block;
+    bottom: 30%;
+  right: 7%;
+  color: blue;
+}
+    </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,5 +2,5 @@
 </script>
 
 <template>
-    <ExactBalance />
+    <NewsTop/>
 </template>
